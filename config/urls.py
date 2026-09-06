71aaaa25296518baeb0596a110a56649829138cc
Robin Hossen <hossenrobin215@gmail.com>
2026-09-07 01:03:04 +0600

    view updated

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 
 from rest_framework_simplejwt.views import (
@@ -22,6 +23,10 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/password-reset/', auth_views.PasswordResetView.as_view(
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt',
+    ), name='password_reset'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
